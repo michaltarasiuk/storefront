e5016636b478278642525c804aff92bc6a451c48
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-07-13 15:20:55 +0200

    refactor: enhance Header component structure and improve navigation layout

--- app/(dashboard)/_components/Header.tsx
+++ app/(dashboard)/_components/Header.tsx
@@ -1,5 +1,3 @@
-import {Avatar} from "@/shared/components/Avatar";
-import {Button} from "@/shared/components/Button";
 import {IconButton} from "@/shared/components/IconButton";
 import {LogoLink} from "@/shared/components/LogoLink";
 import {Routes} from "@/shared/consts/routes";
@@ -12,24 +10,25 @@ import {ProfileSwitcher} from "./ProfileSwitcher";
 export function Header() {
   return (
     <header className={cn("mx-auto max-w-6xl")}>
-      {/* Desktop header */}
+      {/* Desktop navigation */}
       <div className={cn("hidden items-center justify-between py-6 md:flex")}>
-        <nav className={cn("flex items-center gap-12")}>
+        <nav className={cn("gap-large-500 flex items-center")}>
           <LogoLink priority />
-          <MenuItem href={Routes.orders}>Orders</MenuItem>
+          <div>
+            <MenuItem href={Routes.home}>Shop</MenuItem>
+            <MenuItem href={Routes.orders}>Orders</MenuItem>
+          </div>
         </nav>
-        <div className={cn("flex items-center gap-5")}>
-          <ProfileSwitcher />
-          <Button>Go to store</Button>
-        </div>
+        <ProfileSwitcher />
       </div>
-      {/* Mobile header */}
-      <div className={cn("flex items-center justify-between py-5 md:hidden")}>
-        <IconButton aria-label="Open main navigation" className={cn("size-10")}>
+      {/* Mobile navigation */}
+      <div className={cn("relative flex justify-center py-5 md:hidden")}>
+        <IconButton
+          aria-label="Open main navigation"
+          className={cn("absolute start-0 size-10 -translate-x-1/4")}>
           <HamburgerIcon aria-hidden />
         </IconButton>
         <LogoLink priority />
-        <Avatar />
       </div>
     </header>
   );
