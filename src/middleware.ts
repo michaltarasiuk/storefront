75f97ad5a6196d413d201a6b9734ff819296301b
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-07-27 18:02:12 +0200

    refactor: update request path matcher to improve middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,9 @@ export default function middleware(request: Request) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/api(.*)",
   ],
 };
