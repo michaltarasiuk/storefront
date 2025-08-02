32b0f7b2a3ab6c8154da5a25837ecee3df032d64
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-08-02 14:19:57 +0200

    refactor: format import statements for better readability

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import {cva} from "class-variance-authority";
-import {Dialog, type DialogProps, Modal, ModalOverlay} from "react-aria-components";
+import {
+  Dialog,
+  type DialogProps,
+  Modal,
+  ModalOverlay,
+} from "react-aria-components";
 
 import {cn} from "../utils/cn";
 
