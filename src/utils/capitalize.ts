da607723ffa964ac204e34f68f531da868fb212d
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-11 18:29:28 +0200

    simplify capitalize

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,4 +1,3 @@
-export function capitalize(value: string) {
-  const [first = "", ...rest] = value;
+export function capitalize([first = "", ...rest]: string) {
   return first.toUpperCase() + rest.join("").toLowerCase();
 }
